56f0493d10112bb01301c94490eef8dcbd85790a
bansht <natsun974@gmail.com>
2025-05-19 20:31:33 +0800

    update

---
+++ src/app/lalar/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      sdasdasd asd asd jaskljd kasd b saioda skd asd s;al ds; daskl;d <aside>d as
+        d as
+        d 
+        asd 
+        <aside>d as
+             d
+             as
+              das
+               d
+               as
+                daskld a
+                
+        </aside>
+      </aside>
+    </div>
+  )
+}
+
+export default page
